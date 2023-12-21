347de71182568804f5a72b204859cb0d3dee1384
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-21 04:53:17 +0100

    Create & style navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,26 @@
+import './Navbar.css';
+import { SiAnaconda } from "react-icons/si";
+
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className='navbar container'>
+            <div className="logo">
+                <SiAnaconda color='#FFFFFF' size={33} />
+                <p className="logo-text">
+                    Social<span>X</span>
+                </p>
+            </div>
+
+            <menu>
+                <ul className="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Download</a></li>
+                    <li><a href="#">Subscribe</a></li>
+                    <li><a href="#" className='btn btn-dark'>Get Started</a></li>
+                </ul>
+            </menu>
+        </nav>
     )
 }
 
