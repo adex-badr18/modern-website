b7433c95306e748e01ebfd977d185e10517eead2
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-22 04:14:09 +0100

    feat: Make navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import './Navbar.css';
+import { useState } from 'react';
 import { SiAnaconda } from "react-icons/si";
+import { AiOutlineBars } from "react-icons/ai";
+import { RiCloseLine } from "react-icons/ri";
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    function toggleMenu() {
+        setShowMenu(prev => !prev);
+    }
+
     return (
         <nav className='navbar container'>
             <div className="logo">
@@ -12,14 +21,21 @@ const Navbar = () => {
             </div>
 
             <menu>
-                <ul className="nav-links">
+                <ul className="nav-links" id={showMenu ? 'nav-links-mobile' : 'nav-links-mobile-hide'}>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Features</a></li>
                     <li><a href="#">Download</a></li>
                     <li><a href="#">Subscribe</a></li>
-                    <li><a href="#" className='btn btn-dark'>Get Started</a></li>
+                    <li className='nav-btn'><a href="#" className='btn btn-dark'>Get Started</a></li>
                 </ul>
             </menu>
+
+            <div className="menu-icons" onClick={toggleMenu}>
+                {
+                    showMenu ? 
+                    <RiCloseLine color='#FFFFFF' size={30} /> : <AiOutlineBars color='#FFFFFF' size={30} />
+                }
+            </div>
         </nav>
     )
 }
