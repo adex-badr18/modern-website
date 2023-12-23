8a693a7728114db2e7cbf35ce6be49d1274eb8bd
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-23 04:42:04 +0100

    feat: Add & style subscribe section

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,8 +1,35 @@
+import './Subscribe.css';
+import {TiSocialGooglePlus} from 'react-icons/ti';
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
 const Subscribe = () => {
     return (
-        <div>
-            <h1>Subscribe</h1>
-        </div>
+        <section id='subscribe'>
+            <div className="container subscribe">
+                <h2>Subscribe Now!</h2>
+                <form action="">
+                    <div className="form-control">
+                        <input type="text" placeholder='Enter Your Email...' />
+                        <button>Subscribe</button>
+                    </div>
+                </form>
+
+                <div className="social-icons">
+                    <div className="social-icon">
+                        <TiSocialGooglePlus />
+                    </div>
+                    <div className="social-icon">
+                        <FaFacebookF />
+                    </div>
+                    <div className="social-icon">
+                        <FaTwitter />
+                    </div>
+                    <div className="social-icon">
+                        <FaInstagram />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
