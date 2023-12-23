9d291c745ef2eec8258f538d176f94cdd4ca56c0
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-23 04:03:15 +0100

    feat: Add & style download section

--- src/components/download/Download.jsx
+++ src/components/download/Download.jsx
@@ -1,8 +1,35 @@
+import { FaApple, FaWindows } from "react-icons/fa";
+import { GrAndroid } from "react-icons/gr";
+import { IconContext } from "react-icons";
+import './Download.css';
+
 const Download = () => {
     return (
-        <div>
-            <h1>Download</h1>
-        </div>
+        <section id="download">
+            <div className="container download">
+                <h2>SOCIALX MESSENGER DOWNLOAD</h2>
+                <p className="u-text-small u-text-light">
+                    Our apps are available for download on all stores.
+                </p>
+
+                <IconContext.Provider value={{ size: '15' }}>
+                    <div className="download-icons">
+                        <div className="download-icon">
+                            <FaApple />
+                            <p>ios</p>
+                        </div>
+                        <div className="download-icon">
+                            <GrAndroid />
+                            <p>ios</p>
+                        </div>
+                        <div className="download-icon">
+                            <FaWindows />
+                            <p>ios</p>
+                        </div>
+                    </div>
+                </IconContext.Provider>
+            </div>
+        </section>
     )
 }
 
