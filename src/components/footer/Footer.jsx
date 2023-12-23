5176abbee96678af18b11164dc0f8a53e461a17e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-23 13:54:02 +0100

    feat: Add & style footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,59 @@
+import './Footer.css';
+import { FaMapMarkerAlt, FaPhoneAlt, FaFax, FaEnvelope, FaGlobe } from 'react-icons/fa';
+import logo from '/images/logo.png';
+
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer id='footer'>
+            <div className="container footer">
+                <div className="footer-box">
+                    <h4>Useful Links</h4>
+                    <div className="footer-links">
+                        <a href="#">&bull; Support</a>
+                        <a href="#">&bull; About</a>
+                        <a href="#">&bull; Learn</a>
+                        <a href="#">&bull; Hosting</a>
+                        <a href="#">&bull; Messenger</a>
+                    </div>
+                </div>
+                <div className="footer-box">
+                <h4>Support</h4>
+                    <div className="footer-links">
+                        <a href="#">&bull; Support</a>
+                        <a href="#">&bull; About</a>
+                        <a href="#">&bull; Learn</a>
+                        <a href="#">&bull; Hosting</a>
+                        <a href="#">&bull; Messenger</a>
+                    </div>
+                </div>
+                <div className="footer-box">
+                    <h4>Contact Us</h4>
+                    <div className="footer-contact">
+                        <p className='u-text-small u-text-light'>
+                            <FaMapMarkerAlt /> &#160; Address: United States. 
+                        </p>
+                        <p className='u-text-small u-text-light'>
+                            <FaPhoneAlt /> &#160; Phone: +234 806 484 4004  
+                        </p>
+                        <p className='u-text-small u-text-light'>
+                            <FaFax /> &#160; Fax: +1234567890
+                        </p>
+                        <p className='u-text-small u-text-light'>
+                            <FaEnvelope /> &#160; Email: info@socialx.com
+                        </p>
+                        <p className='u-text-small u-text-light'>
+                            <FaGlobe /> &#160; website: www.socialx.com 
+                        </p>
+                    </div>
+                </div>
+                <div className="footer-box">
+                    <img src={logo} alt="SocialX logo" />
+                    <p className="u-text-small">
+                        &copy; Copyright {new Date().getFullYear()}. SocialX.com
+                    </p>
+                </div>
+            </div>
+        </footer>
     )
 }
 
