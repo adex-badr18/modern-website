bf16f181ccec4178954bde558f2379ab79433570
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-23 12:26:35 +0100

    feat:Add & stylefaq section

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -1,8 +1,29 @@
+import './Faq.css';
+import { MdOutlineLibraryBooks } from 'react-icons/md';
+import Question from './Question';
+import { questions } from './data';
+
 const Faq = () => {
     return (
-        <div>
-            <h1>Faq</h1>
-        </div>
+        <section id="features">
+            <div className="container features">
+                <div className="u-title">
+                    <MdOutlineLibraryBooks color="orangered" size={30} />
+                    <h2>FAQS</h2>
+                    <p className="u-text-small u-text-dark">
+                        SocialX Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium doloremque laudantium.
+                    </p>
+                </div>
+
+                <div className="questions">
+                    {
+                        questions.map(question => (
+                            <Question key={question.id} title={question.title} answer={question.answer} />
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
     )
 }
 
---
+++ src/components/faq/Question.jsx
@@ -0,0 +1,35 @@
+import './Question.css';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+import { useState } from 'react';
+
+const Question = ({title, answer}) => {
+    const [showAnswer, setShowAnswer] = useState(false);
+
+    function handleClick() {
+        setShowAnswer(!showAnswer);
+    }
+
+    return (
+        <div className='container question'>
+            <div className="question-title">
+                <h4>{title}</h4>
+                <button className="question-icon" onClick={handleClick}>
+                    {
+                        showAnswer ? 
+                        <AiOutlineMinus color='red' /> :
+                        <AiOutlinePlus color='#1F93FF' />
+                    }
+                </button>
+            </div>
+
+            <div className="question-answer">
+                {
+                    showAnswer &&
+                    <p className="u-text-small u-text-dark">{answer}</p>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Question;
